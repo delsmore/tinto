dabe1a0dd09faccd2a07198175a01b15f7ec9b73
David Elsmore <david.elsmore@ed.ac.uk>
2017-02-03 13:12:54 +0000

    service view

--- testproj/helios/views.py
+++ testproj/helios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
+def service_list(request):
+    return render(request, 'helios/service_list.html', {})
+
+def service_list(request):
+    services = Service.objects.filter().order_by('servicename')
+    return render(request, 'helios/service_list.html', {'services': services})
